d120351dc8ce3c1123df670ac8ed8312dc59fc4f
ash9112 <ashi.mishra9112@gmail.com>
2024-04-16 13:13:19 +0530

    hy

--- src/main/java/com/film/actor/Actorfilm/ActorCon.java
+++ src/main/java/com/film/actor/Actorfilm/ActorCon.java
@@ -1,5 +1,49 @@
 package com.film.actor.Actorfilm;
 
+import org.hibernate.query.Page;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(" /api/actor")
 public class ActorCon {
 
+	
+	@Autowired
+	private ActorService as;
+	
+	@PostMapping 
+	public ResponseEntity<Actor> createFilm(@RequestBody Film film){
+		
+		Actor actor = null;
+		Actor createdFilm = as.createActor(actor);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdFilm);
+		
+	}
+	
+	
+	@GetMapping
+	public ResponseEntity<Page >getAllActor(
+	@RequestParam(defaultValue=" 0") int page,
+	@RequestParam(defaultValue=" 10") int size,
+	@RequestParam(defaultValue=" id") String sortBy){
+		
+	Pageable pg =PageRequest.of(page, size, Sort.by(sortBy))	;
+		
+	}
+	
+	
+	)
+	
+
+	
 }
--- src/main/java/com/film/actor/Actorfilm/FilmCon.java
+++ src/main/java/com/film/actor/Actorfilm/FilmCon.java
@@ -1,5 +1,45 @@
 package com.film.actor.Actorfilm;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(" /api/film")
 public class FilmCon {
+	
+	@Autowired
+	private FilmService fs;
+	
+	@PostMapping 
+	public ResponseEntity<Film> createFilm(@RequestBody Film film){
+		
+		Film createdFilm = fs.createFilm(film);
+		return ResponseEntity.status(HttpStatus.CREATED).body(createdFilm);
+		
+	}
+	
+	
+	public ResponseEntity<Page<Film> > getAllFilm(
+			
+			@RequestParam(defaultValue = " 0")int page;
+			@Requestparam(defaultValue = " 10 ") int size;)
+		
+	{
+		
+		
+		Pageable pagable = PageRequest
+		
+	}
+			
+			
+			
+			)
 
 }
